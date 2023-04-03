c1547dad579fd4ea272c605541c14e3968151de0
jenifferfa <jenifferfa@hotmail.com>
2023-04-02 22:48:31 -0300

    first commit

---
+++ FirstWebServiceSOA/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWebServiceSOA
+{
+    class Program
+    {
+        static void Main()
+        {
+            var demoWebService = new ServiceReference1.CalculatorSoapClient();
+            var result = demoWebService.Multiply(4, 4);
+            Console.WriteLine("Resultado da multiplicação: " + result);
+        }
+    }
+}
